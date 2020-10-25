32d27e468ef3706f53d6ed706fc9c3d9834409c1
cpccharry <harry.subramaniam@cpcc.edu>
2020-10-24 20:57:38 -0400

    Initial commit.

---
+++ src/main/java/edu/lab/gitdemo/SpringbootGitStarterApplication.java
@@ -0,0 +1,13 @@
+package edu.lab.gitdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootGitStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootGitStarterApplication.class, args);
+	}
+
+}
---
+++ src/test/java/edu/lab/gitdemo/SpringbootGitStarterApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.lab.gitdemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootGitStarterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
