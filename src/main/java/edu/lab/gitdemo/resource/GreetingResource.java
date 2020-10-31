16d22fe01d7a6aa41761539bcb0468f0b8aa1d3d
cpccharry <harry.subramaniam@cpcc.edu>
2020-10-31 12:51:09 -0400

    removed logging.

--- src/main/java/edu/lab/gitdemo/resource/GreetingResource.java
+++ src/main/java/edu/lab/gitdemo/resource/GreetingResource.java
@@ -2,8 +2,6 @@ package edu.lab.gitdemo.resource;
 
 import java.util.concurrent.atomic.AtomicLong;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -15,13 +13,10 @@ public class GreetingResource {
 
 	private static final String template = "Greetings, %s!";
     private final AtomicLong counter = new AtomicLong(); // stateful counter...
-    
-    Logger logger = LoggerFactory.getLogger(GreetingResource.class.getName());	
 
     @GetMapping("/api/greetings/greeting")
     public Greeting greeting(@RequestParam(value="name", defaultValue="World") String name) {
     	
-    	logger.debug("greeting: entered...");
     		
     	Greeting greetingObject = null;
 		// the counter value odd (i.e. 1, 3, 5, 7, ...)
@@ -32,8 +27,6 @@ public class GreetingResource {
         	String templateWithName = String.format(template, name);
         	greetingObject = new Greeting(newCounter, templateWithName);
         }
-        
-        logger.debug("greeting: exiting...");
     	
         return greetingObject;
     }
